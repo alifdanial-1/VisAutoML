d5b9a264c47c68d68363d2d3e06f955409e3d669
Muhammad Alif Danial <alifd.nottingham@gmail.com>
2025-03-19 16:35:59 +0800

    Default Version

---
+++ Frontend/src/config/config.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = "https://8341-14-192-210-146.ngrok-free.app";
+export const BACKEND_BASE_URL = "https://8341-14-192-210-146.ngrok-free.app/api/";
+
+// export const API_BASE_URL = "http://localhost:8000";
+// export const BACKEND_BASE_URL = "http://localhost:8000/api/";
