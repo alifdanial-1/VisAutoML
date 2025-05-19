d11643dc3b4d4ee19dfa03afd2fc89f09d457c6b
ZahidChandio <zahidchandio240@gmail.com>
2025-05-19 19:37:14 +0500

    fix: iframe config and deploy script

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = "https://visautomlbackend-production-0d04.up.railway.app";
 // export const BACKEND_BASE_URL = "https://visautomlbackend-production-0d04.up.railway.app/api/";
 
-export const API_BASE_URL = '/';
+export const API_BASE_URL = 'http://52.221.176.156';
 export const BACKEND_BASE_URL = '/api/';
