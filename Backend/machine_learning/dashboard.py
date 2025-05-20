b19bbb6aa33a0cc03c510ade3c0341703088f3e7
ZahidChandio <zahidchandio240@gmail.com>
2025-05-20 19:17:19 +0500

    fix: update deps and dashboard kill command

--- Backend/machine_learning/dashboard.py
+++ Backend/machine_learning/dashboard.py
@@ -5,7 +5,7 @@ import os
 
 def runModel(filename):
     # model = joblib.load('model.joblib')
-    os.system("npx kill-port 8050")
-    os.system("explainerdashboard run explainer.joblib --port 8050 --host 0.0.0.0 &")
+    os.system("sudo kill -9 $(lsof -t -i:8050)")
+    os.system("explainerdashboard run explainer.joblib")
 
     # os.system('explainerdashboard run '+filename+'.joblib')
